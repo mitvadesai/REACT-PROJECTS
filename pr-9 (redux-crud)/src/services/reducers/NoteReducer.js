342717f7f84ae9ee865f50937a2662ef28b0a2a7
mitvadesai <hiraldesai7575@gmail.com>
2025-03-07 11:03:54 +0530

    m

--- pr-9 (redux-crud)/src/services/reducers/NoteReducer.js
+++ pr-9 (redux-crud)/src/services/reducers/NoteReducer.js
@@ -1,61 +1,6 @@
-// const initialState = {
-//     Note: JSON.parse(localStorage.getItem('Note')) || [],
-// }
-
-
-// export const NoteReducer = (state = initalState, action) => {
-//     switch (action.type) {
-//         case "Add_Note":
-//             let oldData = [...state.Note, action.payload];
-//             localStorage.setItem('Note', JSON.stringify(oldData));
-//             return {
-//                 ...state,
-//                 Note: [...state.Note, action.payload],
-               
-//             }
-
-    
-
-//         case "Delete_Note":
-//             let allData = JSON.parse(localStorage.getItem('Note')) || []
-//             let deletedNote = allData.filter((data) => data.id != action.payload)
-//             localStorage.setItem('Note', JSON.stringify(deletedNote));
-//             return {
-//                 ...state,
-//                 Note: deletedNote
-//             }
-            
-//     }
-// }
-
-
-// export const NoteReducer = (state = initialState, action) => {
-//     switch (action.type) {
-//         case "Add_Note":
-//             let updatedNotes = [...state.Note, action.payload];
-//             localStorage.setItem('Note', JSON.stringify(updatedNotes));
-//             return {
-//                 ...state,
-//                 Note: updatedNotes,
-//             };
-
-//         case "Delete_Note":
-//             let filteredNotes = state.Note.filter((data) => data.id !== action.payload);
-//             localStorage.setItem('Note', JSON.stringify(filteredNotes));
-//             return {
-//                 ...state,
-//                 Note: filteredNotes,
-//             };
-
-//         default:
-//             return state; // Always return state if action type is not matched
-//     }
-// };
-
-
 const initialState = {
     Note: JSON.parse(localStorage.getItem('Note')) || [],
-    isCreated: false // ✅ Added `isCreated` flag
+    isCreated: false 
 };
 
 export const NoteReducer = (state = initialState, action) => {
@@ -66,7 +11,7 @@ export const NoteReducer = (state = initialState, action) => {
             return {
                 ...state,
                 Note: updatedNotes,
-                isCreated: true, // ✅ Set flag after adding note
+                isCreated: true, 
             };
 
         case "Delete_Note":
